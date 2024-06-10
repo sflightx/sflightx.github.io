dcb57e8b4ccf800900cac5a2610afad371e217f1
sFlightX <sflightx@gmail.com>
2024-06-10 14:11:51 +0800

    obsat testing

--- rs/missions/config.js
+++ rs/missions/config.js
@@ -1,3 +1,4 @@
 const storedImages = {
-    "omegalink-4_1-mission": "https://i.imgur.com/ou6SgaP.png"
+    "omegalink-4_1-mission": "https://i.imgur.com/ou6SgaP.png",
+    "obsat_1-mission": "https://i.imgur.com/8tT4AxS.png"
   };
